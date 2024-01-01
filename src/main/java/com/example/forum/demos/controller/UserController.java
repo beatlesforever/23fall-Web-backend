f6d033ba842b3ee3a5825cc910b85cff64a17277
llcc01 <2417224420@qq.com>
2024-01-01 19:07:35 +0800

    add static

--- src/main/java/com/example/forum/demos/controller/UserController.java
+++ src/main/java/com/example/forum/demos/controller/UserController.java
@@ -6,7 +6,7 @@ import com.example.forum.demos.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
